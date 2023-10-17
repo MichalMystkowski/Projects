0b6f853bf8b9a55e2cf9a6b0bb62c9f37cd0a04e
Michał Mystkowski <128973538+Mysteques@users.noreply.github.com>
2023-10-17 18:45:08 +0200

    Delete Projekt 3  MonkeyType directory

--- Projekt 3  MonkeyType/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.project {
-
-	requires javafx.base;
-	requires javafx.controls;
-
-	opens org.project to javafx.graphics;
-}
--- Projekt 3  MonkeyType/src/main/java/org/project/Mode.java
+++
@@ -1,6 +0,0 @@
-package org.project;
-
-public enum Mode {
-    APP,
-    SUMMARY
-}
